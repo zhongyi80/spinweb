01752e89f8bad7ef704848f0f6e49245d6b675a7
zhongyi <zhongyi80@hotmail.com>
2024-10-05 21:22:31 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <main className='py-10'>
+      <h1 className='font-bold text-3xl text-center'>
+        hello
+      </h1>
+    </main>
+  )
+}
+
+export default App
