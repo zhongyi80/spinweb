1c8f732dc61f06b159ec68448cb944a150af1f5f
zhongyi <zhongyi80@hotmail.com>
2024-10-13 00:03:28 -0400

    add redux

---
+++ src/Router.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./App";
+import {
+  Dashboard,
+  Team,
+  Invoices,
+  Contacts,
+  Form,
+  Bar,
+  Line,
+  Pie,
+  FAQ,
+  Geography,
+  Calendar,
+  Stream,
+} from "./scenes";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Dashboard />} />
+          
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
