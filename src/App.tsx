aa9c507c87fde009199190905870255648f9e6df
zhongyi <zhongyi80@hotmail.com>
2024-10-13 12:49:34 -0400

    add contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
 import { useState } from 'react'
-
+import { useSelector } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
+import { RootState } from './redux/store';
+import { ConfigProvider, theme as antdTheme } from 'antd';
+
+export const COLOR = {
+  50: '#e0f1ff',
+  100: '#b0d2ff',
+  200: '#7fb0ff',
+  300: '#4d8bff',
+  400: '#1e79fe',
+  500: '#076ee5',
+  600: '#0062b3',
+  700: '#004f81',
+  800: '#003650',
+  900: '#001620',
+  borderColor: '#E7EAF3B2',
+};
 
 function App() {
+  const {mytheme} = useSelector((state: RootState)=>state.theme);
 
   return (
+    <HelmetProvider>
+      <ConfigProvider
+
+      />
+
     <div>
             <h> hello</h>
     </div>
+    </HelmetProvider>
+    
   )
 }
 
